50dc7d7b5179d90e3a99afa3b27625af3c336df4
JVLcode <81806680+jvlcode@users.noreply.github.com>
2025-03-12 21:01:54 +0530

    Initial commit

---
+++ myapp/src/App.jsx
@@ -0,0 +1,25 @@
+import HelloWorld from "./core-concepts/01-helloworld/HelloWorld"
+import Greeting from "./core-concepts/02-components/Greeting"
+import JSXExample from "./core-concepts/03-jsx/JSXExample"
+import PropsExample from "./core-concepts/04-props/PropsExample"
+import StateExample from "./core-concepts/05-state/StateExample"
+import EventHandlingExample from "./core-concepts/06-event-handling/EventHandlingExample"
+import SimpleForm from "./core-concepts/06-event-handling/SimpleForm"
+import ListExample from "./core-concepts/07-list-keys/ListExample"
+import TaskManager from "./core-concepts/08-task-manager/TaskManager"
+// import GreetingClass from './core-concepts/02-components/GreetingClass';
+function App() {
+
+  // return <HelloWorld/>
+  // return <Greeting/>
+  // return <GreetingClass/>
+  // return <JSXExample/>
+  // return <PropsExample technology="Javascript" title="Welcome to JVLCode React Course by JVlogesh"/>
+  // return <StateExample />
+  // return <EventHandlingExample/>
+  // return <SimpleForm/>
+  // return <ListExample/>
+  return <TaskManager />
+}
+
+export default App
